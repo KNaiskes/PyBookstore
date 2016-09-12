3e36e85117c2cebaca9fc33c547a42388ced1cf7
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2016-09-12 20:33:08 +0300

    First commit, it is possible to add a new book - remove a book and view the list of all available books

---
+++ store.py
@@ -0,0 +1,86 @@
+import sqlite3
+
+class BookStore:
+	def __init__(self,title=None,author=None,price=None,isbn=None):
+		self.title = title 
+		self.author = author
+		self.price = price
+		self.isbn = isbn
+
+	def addNewBook(self):
+		conn = sqlite3.connect('books.db')
+		conn.text_factory = str
+		c = conn.cursor()
+		c.execute("CREATE TABLE IF NOT EXISTS books(title TEXT, author TEXT, price REAL,isbn TEXT)")
+		c.execute("INSERT INTO books VALUES(?, ?, ?, ?)",(self.title,self.author,self.price,self.isbn))
+		conn.commit()
+		c.close()
+		conn.close()
+
+	def printDB(self):
+		conn = sqlite3.connect("books.db")
+		conn.text_factory = str
+		c  = conn.cursor()
+		c.execute("SELECT * FROM books")
+		for row in c.fetchall():
+			print(row)
+		conn.commit()
+		c.close()
+		conn.close()
+	
+	def delBook(self,delisbn):
+		conn = sqlite3.connect("books.db")
+		conn.text_factory = str
+		c = conn.cursor()
+		c.execute("SELECT * FROM books WHERE isbn = ?",(delisbn,))
+		count  = c.fetchall()
+		if (len(count) == 0):
+			print("There is no book with isbn :",delisbn)
+		else:
+			c.execute("DELETE FROM books WHERE isbn=?",(delisbn,))
+		conn.commit()
+		c.close()
+		conn.close()
+
+	def menu(self):
+		print("*************************************************************************")
+		print("Enter: 1 to add a new book, 2 to list all books, 3 to remove a book")
+		print("*************************************************************************")
+		option = int(input())
+		if(option == 1):
+			print("Enter book's title:")
+			title = input()
+			print("Enter book's author:")
+			author = input()
+			print("Enter price:")
+			price = input()
+			print("Enter book's ISBN code:")
+			isbn = input()
+			newbook = BookStore(title,author,price,isbn)
+			newbook.addNewBook()
+		elif(option == 2):
+			listStock = BookStore()
+			listStock.printDB()
+		elif(option == 3):
+			print("Enter book's ISBN code that you want to delete:")
+			deleteIsbn = input()
+			deletebook = BookStore()
+			deletebook.delBook(deleteIsbn)
+
+getFunc = BookStore()
+
+getFunc.menu()
+
+while(True):
+	print()
+	print()
+	print("Do you want to do more actions ? y/n :")
+	answ = input()
+	if(answ == "y"):
+		getFunc.menu()
+	elif(answ == "n"):
+		print("GoodBye!")
+		break
+	else:
+		print("Unknown command. Try again")
+
